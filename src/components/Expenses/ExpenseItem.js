35283904ebbf649e8a8e6cae6dc288d984b524b1
nadirbashir <1.nadirbashir@gmail.com>
2021-07-06 21:49:07 +0500

    module-5/Expense Tracker Complete complete

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,21 @@
-import './ExpenseItem.css';
-import { Card } from '../UI/Card';
-import { ExpenseDate } from './ExpenseDate';
+import "./ExpenseItem.css";
+import { Card } from "../UI/Card";
+import { ExpenseDate } from "./ExpenseDate";
 
 export const ExpenseItem = (props) => {
-  // state is seperated as per component instance basis 
+  // state is seperated as per component instance basis
   console.log("ExpenseItem evaluated by react");
   // let title = props.title;
-  
 
-    return (
-    <Card className="expense-item">
-        <ExpenseDate date = {props.date} />
-      <div className = "expense-item__description">
-        <h2>{props.title}</h2>
-        <div className ="expense-item__price">{props.amount}</div>
-      </div>
-    </Card>
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
-import { ExpenseItem } from "./ExpenseItem";
+import { ExpensesList } from "./ExpensesList";
 import { Card } from "../UI/Card";
 import { ExpensesFilter } from "./ExpensesFilter";
+import { ExpensesChart } from "./ExpensesChart";
 import './Expenses.css'
 import { useState } from "react";
 export const Expenses = (props) => {
@@ -12,16 +13,12 @@ export const Expenses = (props) => {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expensesContent = <p>No Expense Found in {filteredYear} </p>
-  // Cleanest way outputting conditional content
-  if(filteredExpenses.length){
-    expensesContent = filteredExpenses.map(expense => <ExpenseItem key = {expense.id} title = {expense.title} amount = {expense.amount} date = {expense.date} />)
-  }
+  
   return (
     <Card className="expenses">
       <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
-
-      {expensesContent}   
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items = {filteredExpenses} />
       {/* {filteredExpenses.length === 0 && <p>No Expense Found in {filteredYear}</p>}
       {filteredExpenses.length > 0 && filteredExpenses.map(expense => <ExpenseItem key = {expense.id} title = {expense.title} amount = {expense.amount} date = {expense.date} />)} */}
       
